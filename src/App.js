719202d4f2a7a19e1bbebb1619bf38cc1c5aff74
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-03-21 20:18:12 +0100

    added clean start-files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
-//* сборка основного компонента
+// function App() {
+//   return (
+//   );
+// }
 
-import paintings from './paintings.json'; // коллекция эл-ов
-import PaintingList from './components/PaintingList'; // передача пропсов с коллекции в компонент
-import Section from './components/Section';
-
-export default function App() {
-  return (
-    <div>
-      {/* в секцию передаем заголовок и коллекцию компонентов */}
-      <Section title="Топ недели">
-        <PaintingList items={paintings} />
-      </Section>
-
-      {/* в секцию передаем заголовок  */}
-      <Section title="Лучшее"></Section>
-    </div>
-  );
-}
+// export default App;
