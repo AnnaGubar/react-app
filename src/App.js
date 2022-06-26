9a7fae65d9e60192b5680dcddf318e66aa693e5b
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-06-26 08:41:08 +0300

    adds files
    aren't fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
-// function App() {
-//   return (
-//   );
-// }
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import TodosView from './views/TodosView';
+import Counter from './components/Counter';
 
-// export default App;
+const App = () => (
+  <>
+    <ul>
+      <li>
+        <Link to="/counter">Счётчик</Link>
+      </li>
+      <li>
+        <Link to="/todos">Заметки</Link>
+      </li>
+    </ul>
+
+    <Switch>
+      <Route path="/counter">
+        <Counter />
+      </Route>
+
+      <Route path="/todos">
+        <TodosView />
+      </Route>
+    </Switch>
+  </>
+);
+
+export default App;
